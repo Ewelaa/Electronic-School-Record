ac1c078429e62a3e21dc42f564e533fd0fb54562
Ewelina Blicharz <eweliblicharz@gmail.com>
2016-03-18 17:11:38 +0100

    class user and others v.2

---
+++ src/esr/Teacher.java
@@ -0,0 +1,26 @@
+package esr;
+
+public class Teacher extends User {
+	private String name;
+	private String surname;
+	private String password;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setSurname(String surname){
+		this.surname = surname;
+	}
+	public void setPassword(String password){
+	this.password = password;
+	}
+	public String getName(){
+		return name;
+	}
+	public String getSurname(){
+		return surname;
+	}
+	public String getPassword(){
+		return password;
+	}
+}
---
+++ src/esr/User.java
@@ -0,0 +1,25 @@
+package esr;
+
+public abstract class User {
+ private String name;
+ private String surname;
+ private String password;
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setSurname(String surname){
+		this.surname = surname;
+	}
+	public void setPassword(String password){
+		this.password = password;
+	}
+	public String getName(){
+		return name;
+	}
+	public String getSurname(){
+		return surname;
+	}
+	public String getPassword(){
+		return password;
+	}
+}
