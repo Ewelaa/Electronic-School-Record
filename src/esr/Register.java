0eff1b2cd76ebc6e4345da1e6ad40611800e2e69
Ewelina Blicharz <eweliblicharz@gmail.com>
2016-04-08 13:36:19 +0200

    small fix

--- src/esr/Register.java
+++ src/esr/Register.java
@@ -7,14 +7,14 @@ public class Register {
 	private HashMap<Student, HashMap<Subject, SubjectSituation>> studentSubjectDictionary = 
 			new HashMap<Student, HashMap<Subject, SubjectSituation>>();
 
-
-	public HashMap<Student, HashMap<Subject, SubjectSituation>> getStudentSubjectDictionary() {
+	public HashMap<Student, HashMap<Subject, SubjectSituation>> getStudentSubjectDictionary() 
+	{
 		return studentSubjectDictionary;
 	}
 
-	public void setStudentSubjectDictionary(HashMap<Student, HashMap<Subject, SubjectSituation>> studentSubjectDictionary) {
+	public void setStudentSubjectDictionary(HashMap<Student, HashMap<Subject, SubjectSituation>> studentSubjectDictionary) 
+	{
 		this.studentSubjectDictionary = studentSubjectDictionary;
 	}
 	
-
 }
